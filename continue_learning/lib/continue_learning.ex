6f1f510b6c0f9edd9e58b6b81ddcafd9545b5155
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2023-11-21 04:37:54 +0000

    chore: preparing mix project

---
+++ continue_learning/lib/continue_learning.ex
@@ -0,0 +1,18 @@
+defmodule ContinueLearning do
+  @moduledoc """
+  Documentation for `ContinueLearning`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ContinueLearning.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ continue_learning/test/continue_learning_test.exs
@@ -0,0 +1,8 @@
+defmodule ContinueLearningTest do
+  use ExUnit.Case
+  doctest ContinueLearning
+
+  test "greets the world" do
+    assert ContinueLearning.hello() == :world
+  end
+end
